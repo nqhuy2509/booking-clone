2f04869794a3dcf6aa526269960b347382615945
thanhlocpttl <113110992+thanhlocpttl@users.noreply.github.com>
2024-07-06 10:21:20 +0700

    commit

--- src/app/(main)/_components/NavBar.tsx
+++ src/app/(main)/_components/NavBar.tsx
@@ -1,55 +1,68 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { Navbar, NavbarBrand, NavbarContent, Button, Image } from '@nextui-org/react'
-import Language from './Language'
-import Login from './Login'
-import Register from './Register'
-import { useAppSelector } from '../../../redux/store'
-import UserProfile from './UserProfile'
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  Button,
+  Image,
+} from "@nextui-org/react";
+import Language from "./Language";
+import Login from "./Login";
+import Register from "./Register";
+import { useAppSelector } from "../../../redux/store";
+import UserProfile from "./UserProfile";
 
 export default function NavBar() {
-	const authState = useAppSelector((state) => state.auth)
+  const authState = useAppSelector((state) => state.auth);
 
-	return (
-		<>
-			<div>
-				<Navbar maxWidth='2xl' position='sticky' className='bg-primary'>
-					<NavbarContent justify='start'>
-						<NavbarBrand>
-							<Image src='/svg/bookingcom-1.svg' width={50} />
-							<div className='text-white font-bold p-1 text-2xl'> Booking.com</div>
-						</NavbarBrand>
-					</NavbarContent>
-					<NavbarContent justify='end' className='flex'>
-						<div className='text-white font-bold'>VND</div>
-						<Language></Language>
-						<Button className='p-0 bg-transparent ' isIconOnly>
-							<Image
-								src='/svg/question-mark-1750942_640.webp'
-								width={30}
-								height={30}
-								className=''
-							/>
-						</Button>
+  return (
+    <>
+      <div>
+        <Navbar maxWidth="2xl" position="sticky" className="bg-primary">
+          <NavbarContent justify="start">
+            <NavbarBrand>
+              <Image src="/svg/bookingcom-1.svg" width={50} />
+              <div className="text-white font-bold p-1 text-2xl">
+                {" "}
+                Booking.com
+              </div>
+            </NavbarBrand>
+          </NavbarContent>
+          <NavbarContent justify="end" className="flex">
+            <div className="text-white font-bold">VND</div>
+            <Language></Language>
+            <Button className="p-0 bg-transparent " isIconOnly>
+              <Image
+                src="/svg/question-mark-1750942_640.webp"
+                width={30}
+                height={30}
+                className=""
+              />
+            </Button>
 
-						{!authState.isAuthenticated ? (
-							<>
-								<Button className='text-white font-semibold p-1 text-xl bg-transparent'>
-									{' '}
-									Đăng chổ nghỉ của Quý vị{' '}
-								</Button>
-								<Register />
-								<Login />
-							</>
-						) : (
-							<>
-								<UserProfile />
-							</>
-						)}
-					</NavbarContent>
-				</Navbar>
-			</div>
-		</>
-	)
+            {!authState.isAuthenticated ? (
+              <>
+                <a
+                  href="https://travel-booking-console.vercel.app/auth/login?fbclid=IwZXh0bgNhZW0CMTEAAR1L49C9vHrwsHJHq8s8AJOjgII5TEas0uxzEaCdu81YHC5duODOKLsUFU4_aem_htoCU_lkqFS9Sq0uMHyU_g"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-white font-semibold p-1 text-xl bg-transparent"
+                >
+                  Đăng chổ nghỉ của Quý vị
+                </a>
+                <Register />
+                <Login />
+              </>
+            ) : (
+              <>
+                <UserProfile />
+              </>
+            )}
+          </NavbarContent>
+        </Navbar>
+      </div>
+    </>
+  );
 }
--- src/app/(main)/_components/UserProfile.tsx
+++ src/app/(main)/_components/UserProfile.tsx
@@ -1,85 +1,87 @@
-import React from 'react'
-import { useAppDispatch, useAppSelector } from '../../../redux/store'
+import React from "react";
+import { useAppDispatch, useAppSelector } from "../../../redux/store";
 import {
-	Button,
-	Modal,
-	ModalBody,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	Popover,
-	PopoverContent,
-	PopoverTrigger,
-	Select,
-	SelectItem,
-	Tooltip,
-	useDisclosure,
-	User,
-} from '@nextui-org/react'
-import { logout } from '../../../redux/auth/authSlice'
-import { clearStorage } from '../../../utils/storage'
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  useDisclosure,
+  User,
+} from "@nextui-org/react";
+import { logout } from "../../../redux/auth/authSlice";
+import { clearStorage } from "../../../utils/storage";
 
 const UserProfile = () => {
-	const authState = useAppSelector((state) => state.auth)
+  const authState = useAppSelector((state) => state.auth);
 
-	const { isOpen, onOpen, onOpenChange } = useDisclosure()
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-	const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-	const handleLogout = () => {
-		dispatch(logout())
-		clearStorage()
-	}
+  const handleLogout = () => {
+    dispatch(logout());
+    clearStorage();
+  };
 
-	return (
-		<>
-			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-				<ModalContent>
-					{(onClose) => (
-						<>
-							<ModalHeader className='flex flex-col gap-1'>Đăng xuất</ModalHeader>
-							<ModalBody>Bạn có chắc chắn muốn đăng xuất không?</ModalBody>
-							<ModalFooter>
-								<Button color='primary' variant='light' onPress={onClose}>
-									Huỷ
-								</Button>
-								<Button
-									color='danger'
-									onPress={() => {
-										onClose()
-										handleLogout()
-									}}>
-									Đăng xuất
-								</Button>
-							</ModalFooter>
-						</>
-					)}
-				</ModalContent>
-			</Modal>
-			<Popover>
-				<PopoverTrigger>
-					<User name={authState.email} className='text-white' />
-				</PopoverTrigger>
-				<PopoverContent>
-					<ul className='w-full py-1'>
-						<Button
-							color='primary'
-							variant='light'
-							className='w-full cursor-pointer hover:bg-primary-300 px-4 py-2 rounded-md text-sm font-semibold my-2'>
-							Lịch sử đặt phòng
-						</Button>
-						<Button
-							color='danger'
-							variant='light'
-							className='w-full cursor-pointer hover:bg-primary-300 px-4 py-2 rounded-md text-sm font-semibold'
-							onClick={onOpen}>
-							Đăng xuất
-						</Button>
-					</ul>
-				</PopoverContent>
-			</Popover>
-		</>
-	)
-}
+  return (
+    <>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Đăng xuất
+              </ModalHeader>
+              <ModalBody>Bạn có chắc chắn muốn đăng xuất không?</ModalBody>
+              <ModalFooter>
+                <Button color="primary" variant="light" onPress={onClose}>
+                  Huỷ
+                </Button>
+                <Button
+                  color="danger"
+                  onPress={() => {
+                    onClose();
+                    handleLogout();
+                  }}
+                >
+                  Đăng xuất
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+      <Popover>
+        <PopoverTrigger>
+          <User name={authState.email} className="text-white" />
+        </PopoverTrigger>
+        <PopoverContent>
+          <ul className="w-full py-1">
+            <Button
+              color="primary"
+              variant="light"
+              className="w-full cursor-pointer hover:bg-primary-300 px-4 py-2 rounded-md text-sm font-semibold my-2"
+            >
+              Lịch sử đặt phòng
+            </Button>
+            <Button
+              color="danger"
+              variant="light"
+              className="w-full cursor-pointer hover:bg-primary-300 px-4 py-2 rounded-md text-sm font-semibold"
+              onClick={onOpen}
+            >
+              Đăng xuất
+            </Button>
+          </ul>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
 
-export default UserProfile
+export default UserProfile;
