b2d1ed6faf5c66991a975123bc72a9c4cd3fdeea
@quanhuy <64772864+nqhuy2509@users.noreply.github.com>
2024-07-08 23:11:21 +0700

    integrate data to history

--- src/api/bookingApi.ts
+++ src/api/bookingApi.ts
@@ -7,6 +7,9 @@ const bookingApi = {
 	postNewBooking: async (data: BookingRequest): Promise<IResponseAPI<BookingModel>> => {
 		return await apiClient.post('api/v1/booking', data)
 	},
+	getBookingHistory: async (): Promise<IResponseAPI<BookingModel[]>> => {
+		return await apiClient.get('api/v1/booking')
+	},
 }
 
 export default bookingApi
--- src/app/(main)/history_booking/page.tsx
+++ src/app/(main)/history_booking/page.tsx
@@ -1,64 +1,70 @@
-import React from "react";
-import Footer from "../_components/Footer";
+'use client'
+
+import React, { useCallback, useEffect, useState } from 'react'
+import Footer from '../_components/Footer'
+import { BookingModel } from '../../../interfaces/booking.model'
+import bookingApi from '../../../api/bookingApi'
+import { formatCurrency } from '../../../utils/utils'
 
 const BookingHistory = () => {
-  const bookings = [
-    {
-      bookingId: 1,
-      hotelName: "Hotel Example 1",
-      roomNumber: "101",
-      checkInDate: "2023-07-01",
-      checkOutDate: "2023-07-05",
-      totalPrice: 500000,
-    },
-    {
-      bookingId: 2,
-      hotelName: "Hotel Example 2",
-      roomNumber: "202",
-      checkInDate: "2023-08-10",
-      checkOutDate: "2023-08-15",
-      totalPrice: 750000,
-    },
-  ];
+	const [bookings, setBookings] = useState<BookingModel[]>()
 
-  return (
-    <div className="max-w-[1024px] mx-auto mt-4">
-      <div className="bg-white rounded-lg shadow-md overflow-hidden">
-        <div className="px-6 py-4 border-b border-gray-200">
-          <h1 className="text-3xl font-bold">Lịch Sử Đặt Phòng</h1>
-        </div>
+	const fetchBookingHistories = useCallback(async () => {
+		try {
+			const response = await bookingApi.getBookingHistory()
 
-        <div className="px-6 py-4">
-          {bookings.length === 0 ? (
-            <p>Không có lịch sử đặt phòng.</p>
-          ) : (
-            <ul>
-              {bookings.map((booking) => (
-                <li key={booking.bookingId} className="mb-4">
-                  <div className="bg-gray-100 p-4 rounded-lg">
-                    <h2 className="text-xl font-semibold">
-                      {booking.hotelName}
-                    </h2>
-                    <p>Phòng: {booking.roomNumber}</p>
-                    <p>
-                      Ngày nhận phòng:{" "}
-                      {new Date(booking.checkInDate).toLocaleDateString()}
-                    </p>
-                    <p>
-                      Ngày trả phòng:{" "}
-                      {new Date(booking.checkOutDate).toLocaleDateString()}
-                    </p>
-                    <p>Tổng cộng: VND {booking.totalPrice.toLocaleString()}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
-};
+			if (response.success) {
+				setBookings(response.data)
+			}
+		} catch (error) {}
+	}, [])
 
-export default BookingHistory;
+	useEffect(() => {
+		fetchBookingHistories()
+	}, [fetchBookingHistories])
+
+	return (
+		<div className='max-w-[1024px] mx-auto mt-4'>
+			<div className='bg-white rounded-lg shadow-md overflow-hidden'>
+				<div className='px-6 py-4 border-b border-gray-200'>
+					<h1 className='text-3xl font-bold'>Lịch Sử Đặt Phòng</h1>
+				</div>
+
+				<div className='px-6 py-4'>
+					{bookings != null && bookings.length === 0 ? (
+						<p>Không có lịch sử đặt phòng.</p>
+					) : (
+						<ul>
+							{bookings != null &&
+								bookings.map((booking) => (
+									<li key={booking.id} className='mb-4'>
+										<div className='bg-gray-100 p-4 rounded-lg'>
+											<h2 className='text-xl font-semibold'>
+												{booking.room?.name}
+											</h2>
+											<p>Phòng: {booking.room?.beds} giường</p>
+											<p>
+												Ngày nhận phòng:{' '}
+												{new Date(booking.startDate).toLocaleDateString()}
+											</p>
+											<p>
+												Ngày trả phòng:{' '}
+												{new Date(booking.endDate).toLocaleDateString()}
+											</p>
+											<p>
+												Tổng cộng:{' '}
+												{formatCurrency(booking.room?.price || 0)}
+											</p>
+										</div>
+									</li>
+								))}
+						</ul>
+					)}
+				</div>
+			</div>
+			<Footer />
+		</div>
+	)
+}
+
+export default BookingHistory
